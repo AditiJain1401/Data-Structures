5b0432dcd8497c8f3a3a22f7442721ab2161285c
Aditi Jain <88366504+AditiJain1401@users.noreply.github.com>
2021-11-02 14:16:09 +0530

    Update queue.js

--- queue.js
+++ queue.js
@@ -3,34 +3,34 @@ Queue: works on FIFO(First In First Out) */
 
 class Queue {
     constructor() {
-        this.item = []
-        this.count = 0
+        this._storage = []
+        this._count = 0
     }
 
-    //Push function - returns the length of the stack
-    push(element) {
-        this.item[this.count] = element;
-        this.count += 1;
-        return this.count;
+    //Enqueue function - returns the length of the stack
+    enqueue(element) {
+        this._storage[this._count] = element;
+        this._count ++;
+        return this._count;
     }
 
-    //Pop function - returns the deleted element from end of the stack
-    pop() {
-        if (this.count == 0) {
-            return "NULL! Queue is Empty"; //returns null if stack is empty
+    //Dequeue function - returns the deleted element from end of the stack
+    dequeue() {
+        if (this._count == 0) {
+            return "NULL"; //returns null if stack is empty
         } else {
-            let Element = this.item[0];
-            delete this.item[0]
-            this.item[0] = this.item[1]
-            this.count -= 1
-            return Element;
+            let element = this._storage[0];
+            delete this._storage[0]
+            this._storage[0] = this._storage[1]
+            this._count -= 1
+            return element;
         }
     }
 }
 const queue = new Queue();
 console.log(queue);
-console.log(queue.push(1));
-console.log(queue.push(2));
-console.log(queue.pop());
-console.log(queue.pop());
-console.log(queue.pop());
+console.log(queue.enqueue(1));
+console.log(queue.enqueue(2));
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
