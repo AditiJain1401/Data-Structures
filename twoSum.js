27ce05944cf229319c16f7f6c975067b17306465
Aditi Jain <88366504+AditiJain1401@users.noreply.github.com>
2021-11-08 13:15:28 +0530

    Add files via upload

---
+++ twoSum.js
@@ -0,0 +1,17 @@
+function twoSum(arr, target) {
+  //[3,2,4] target:6
+  let index = 0; //index =0
+  const map = {}; //map{}
+  while (index < arr.length) {
+    //true, true, true
+    const remaining = target - arr[index]; //0-3=-3
+    if (remaining in map) {
+      //false, false, true
+      return [index, map[remaining]]; //[2,1]
+    }
+    map[arr[index]] = index; //{3:0}, {3:0,2:1}
+
+    index += 1; //index:1, index:2
+  }
+}
+console.log(twoSum([-3, 4, 3, 90], 0));
