41c61f3e27cbe06cc40b3fe813fc5a30c7412958
Aditi Jain <88366504+AditiJain1401@users.noreply.github.com>
2021-11-22 15:48:58 +0530

    Update myIndexOf.js

--- myIndexOf.js
+++ myIndexOf.js
@@ -1,5 +1,5 @@
 Array.prototype.myIndexOf = function (element) {
-  for (let _i = 0; i < this.length; i++) {
+  for (let i = 0; i < this.length; i++) {
     if (this[i] == element) {
       return i;
     }
