cca6f9bee7e53a1de45ec9e186bb752a670ddd39
Aditi Jain <88366504+AditiJain1401@users.noreply.github.com>
2021-11-22 15:43:59 +0530

    Delete runningSum.js

--- runningSum.js
+++
@@ -1,9 +0,0 @@
-function runningSum(nums) {
-  const newArray = [];
-  newArray.push(nums[0]);
-  for (let i = 1; i < nums.length; i++) {
-    const lastElement = newArray[newArray.length - 1];
-    newArray.push(lastElement + nums[i]);
-  }
-  return newArray;
-}
